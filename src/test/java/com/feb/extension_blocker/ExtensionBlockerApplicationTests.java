f0158b3e6cc60de5c0d664b2c65ee9c12cc42ad6
ZeroZoa <roh100438@gmail.com>
2026-08-28 19:09:59 +0900

    chore: init project via Spring Initializr

---
+++ src/test/java/com/feb/extension_blocker/ExtensionBlockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feb.extension_blocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtensionBlockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
